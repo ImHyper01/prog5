18a39b0d918f28bc4225b6b87ce2383b6a5eb3bb
ImHyper01 <57798860+ImHyper01@users.noreply.github.com>
2022-09-29 15:15:46 +0200

    inlogsysteem erin gezet

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,23 @@
-<h2>Hallo World</h2>
+@extends('layouts.app')
 
-<li>
-    <a href="inlog.blade.php">inlog test</a>
-</li>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-<li>
-    <a href="product.blade.php">product test</a>
-</li>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/home', 'App\Http\Controllers\home@index');
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
